90fef8080c973cca701e32d57fd4cf740c915640
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-17 14:52:24 +0530

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -29,8 +29,7 @@ export default class Command extends BaseCommand {
                 videos[i].author.name
             }\n⏳ *Duration:* ${videos[i].duration}\n💸 *URL:* ${videos[i].url}\n\n`
         }
-        M.reply('_*Loading…
-                █▒▒▒▒▒▒▒▒▒*_')
+        M.reply('_*︵‿୨♡𝓛𝓸𝓪𝓭𝓲𝓷𝓰♡୧‿︵*_')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
