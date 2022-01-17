24422184bd4cae18cc443afc336b5fc0e047039c
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-17 10:55:17 +0530

    Update do-you-mean.ts

--- src/commands/General/do-you-mean.ts
+++ src/commands/General/do-you-mean.ts
@@ -14,10 +14,10 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            './assets/zerotwo-mean.mp4'
+            './assets/lisa-mean.mp4'
         ]
-        let well = n[Math.floor(Math.random() * n.length)]
-        return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
+        let pain = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: pain }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
             caption: `𝘿𝙖𝙧𝙡𝙞𝙣' 𝙩𝙮𝙥𝙚 🐥 ${this.client.config.prefix}ᴡᴇʟʟ\n` }
         )
