8413be1d25690db6f585e8f15d9c243b446c251e
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-19 19:27:34 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -16,6 +16,7 @@ export default class EventHandler {
         const data = await this.client.getGroupData(event.jid)
         if (!data.events) return void null
         const add = event.action === 'add'
+        const remove = event.action === 'remove'
         const text = add
             ? `✆ 𝙒𝙚𝙡𝙘𝙤𝙢𝙚 𝙩𝙤 - ${group.subject || '___'} -\n\n🍁ɢʀᴏᴜᴘ ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ:\n${
                group.desc
@@ -24,7 +25,7 @@ export default class EventHandler {
               .join(', ')}*`
               
             : event.action === 'remove'
-            ? `*@${event.participants[0].split('@')[0]}* ʜᴀꜱ ʟᴇꜰᴛ ᴛʜᴇ ᴄʜᴀᴛ ᴀɴᴅ ʙᴇᴄᴀᴜꜱᴇ ɴᴏᴡ ʏᴏᴜ'ʀᴇ ᴊᴜꜱᴛ ʟɪᴋᴇ ᴛʜᴀᴛ. ɪᴛ'ꜱ ɴᴏᴛ ᴄᴏᴏʟ 👠`
+            ? `*@${event.participants[0].split('@')[0]}* ᴀɴɴʏᴇᴏɴɢ 👋🏻`
             : `*@${event.participants[0].split('@')[0]}* got ${this.client.util.capitalize(event.action)}d${
                   event.actor ? ` by *@${event.actor.split('@')[0]}*` : ''
               }`
@@ -33,6 +34,16 @@ export default class EventHandler {
         }
         if (add) {
             let image = await this.client.assets.get('welcome1')
+            
+            if (image)
+                return void (await this.client.sendMessage(event.jid, image, MessageType.video, {
+                    caption: text,
+                    mimetype: Mimetype.gif,
+                    contextInfo
+                }))
+        }
+        if (remove) {
+            let image = await this.client.assets.get('goodbye1')
             
             if (image)
                 return void (await this.client.sendMessage(event.jid, image, MessageType.video, {
