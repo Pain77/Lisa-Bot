5bd7d901d01960909558cd04da2bf23c5f0e2b36
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-20 14:51:44 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -1,23 +1,22 @@
 /** @format */
 
-import { MessageType, Mimetype } from "@adiwajshing/baileys";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import { Sticker, Categories, StickerTypes } from "wa-sticker-formatter";
 
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
-			command: "broadcast",
-			description:
-				"Will make a broadcast for groups where the bot is in. Can be used to make announcements.",
-			aliases: ["bcast", "announcement", "bc"],
-			category: "dev",
-			dm: true,
-			usage: `${client.config.prefix}bc`,
-			modsOnly: true,
-			baseXp: 0,
+			command: "everyone",
+			description: "Tags all users in group chat",
+			aliases: ["all", "tagall", "ping"],
+			category: "moderation",
+			usage: `${client.config.prefix}everyone`,
+			adminOnly: true,
+			baseXp: 20,
 		});
 	}
 
@@ -25,33 +24,29 @@ export default class Command extends BaseCommand {
 		M: ISimplifiedMessage,
 		{ joined }: IParsedArgs
 	): Promise<void> => {
-		if (!joined)
-			return void (await M.reply(`Please provide the Broadcast Message.`));
-		const term = joined.trim();
-		const gifs = [
-			"https://c.tenor.com/lq3GJ0FrmG8AAAPo/lisa-blackpink-lisa-swalla.mp4",
-			"https://c.tenor.com/OGupI21dlwYAAAPo/lalisa-money.mp4",
-			"https://c.tenor.com/T-TVk5AGH_MAAAPo/rap-blackpink.mp4",
-			"https://c.tenor.com/wy9JHWiGlZwAAAPo/girl-power-blackpink.mp4",
+		const gifs = [		
+			"https://c.tenor.com/XVLRX-3bx6MAAAPo/lisa-cute.mp4",
 		];
-		const selected = gifs[Math.floor(Math.random() * gifs.length)];
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const chats: any = this.client.chats
-			.all()
-			.filter((v) => !v.read_only && !v.archive)
-			.map((v) => v.jid)
-			.map((jids) => (jids.includes("g.us") ? jids : null))
-			.filter((v) => v);
-		for (let i = 0; i < chats.length; i++) {
-			const text = `*🥤「 Lisa Broadcast 」🥤*\n\n*🧧 Message:* ${term}\n*☘️ Regards:* *${M.sender.username}*`;
-			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
-				mimetype: Mimetype.gif,
-				caption: `${text}`,
-				contextInfo: {
-					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
-				},
-			});
-		}
-		await M.reply(` Broadcast Message sent to *${chats.length} groups*.`);
-	};
+
+	        const random = gifs[Math.floor(Math.random() * gifs.length)];
+		
+		return void (await M.reply(
+				MessageType.video,
+				Mimetype.gif,
+				M.groupMetadata?.participants.map((user) => user.jid)
+		));
+	                
+	} else
+		return void (await M.reply(
+				`${
+					M.groupMetadata?.subject || "*EVERYONE*"
+				}\n*READ QUOTED MESSAGE*\n*[TAGGED MAGICALLY]*`,
+				undefined,
+				undefined,
+				M.groupMetadata?.participants.map((user) => user.jid)
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			).catch((reason: any) =>
+				M.reply(`✖️ An error occurred, Reason: ${reason}`)
+			));
+	        };
 }
